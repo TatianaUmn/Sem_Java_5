1d9fe53af8ef596bb1759c3bb786322ff6aaa20d
Tatiana <tatum@rambler.ru>
2022-12-18 14:09:56 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // SemFiveTaskOneDz semFiveTaskOneDz = new SemFiveTaskOneDz();
+        // semFiveTaskOneDz.findInPhoneBook();
+
+        // SemFiveTaskTwoDz semFiveTaskTwoDz = new SemFiveTaskTwoDz();
+        // semFiveTaskTwoDz.fillNameMap();
+        // Map<String, Integer> nameMap = semFiveTaskTwoDz.fillNameMap();
+        // System.out.println("Список повторяющихся имен с их количеством : ");
+        // System.out.println(nameMap);
+        // System.out.println("Отсортированный список: ");
+        // semFiveTaskTwoDz.sortedNameMap(nameMap);
+
+        SemFiveTaskThreeDz semFiveTaskThreeDz = new SemFiveTaskThreeDz();
+        semFiveTaskThreeDz.result();
+        
+
+        
+        
+    }
+    
+}
